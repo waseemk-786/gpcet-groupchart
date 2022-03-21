297b91b33dfcba0f693b3790b70bee309c7acfb8
Waseem k <you@example.com>
2022-03-21 19:17:56 +0530

    web server program

---
+++ mqtt_pub.py
@@ -0,0 +1,8 @@
+import paho.mqtt.client as mqtt
+
+pub_client=mqtt.Client()
+pub_client.connect('broker.hivemq.com',1883)
+print('Broker Connected')
+
+pub_client.publish('gpcet/ai','I am waseem')
+
